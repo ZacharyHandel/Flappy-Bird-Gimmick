b3b725f42bc4c34041b1731bf0217d63e1a6f69b
Zachary Handel <zachary.handelzh@gmail.com>
2022-12-11 16:32:05 -0600

    Update Move Script and Add Spawner

--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeMoveScript : MonoBehaviour
+public class PipeSpawnerScript : MonoBehaviour
 {
-
-    public float moveSpeed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,6 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        
     }
 }
